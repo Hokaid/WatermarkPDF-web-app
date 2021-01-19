c995d646c5459a44551c91a48f7c5481d85af321
Geral Esteen Castillo Arredondo <39970728+Hokaid@users.noreply.github.com>
2021-01-19 16:12:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, render_template, request, send_file, url_for, redirect
 from flask_dropzone import Dropzone
-import PyPDF2
+import PyPDF4
 from flask import flash
 from os import listdir
 from os.path import isfile, join
@@ -10,10 +10,10 @@ import shutil
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-def put_watermark(input_pdf, output_pdf, watermark): 
-    template = PyPDF2.PdfFileReader(open(input_pdf, 'rb'))
-    watermark = PyPDF2.PdfFileReader(open(watermark, 'rb'))
-    output = PyPDF2.PdfFileWriter()
+def put_watermark(input_pdf, output_pdf, watermark):
+    template = PyPDF4.PdfFileReader(input_pdf)
+    watermark = PyPDF4.PdfFileReader(watermark)
+    output = PyPDF4.PdfFileWriter()
     for i in range(template.getNumPages()):
         page = template.getPage(i)
         page.mergePage(watermark.getPage(0))
@@ -22,7 +22,7 @@ def put_watermark(input_pdf, output_pdf, watermark):
         output.write(file)
 
 def eliminar_archivos(path):
-    for f in os.listdir(path): 
+    for f in os.listdir(path):
         os.remove(os.path.join(path, f))
 
 def comprobar_envio(watermark, upfiles):
@@ -48,10 +48,10 @@ def upload():
         f = request.files.get('file')
         f.save(os.path.join(app.config['UPLOADED_PATH'],f.filename))
     else:
-        eliminar_archivos('output/')
-        eliminar_archivos('uploads/')
-        eliminar_archivos('watermark/')
-        if os.path.exists("resultado.zip"): os.remove("resultado.zip")
+        eliminar_archivos(os.path.join(basedir,'output/'))
+        eliminar_archivos(os.path.join(basedir,'uploads/'))
+        eliminar_archivos(os.path.join(basedir,'watermark/'))
+        if os.path.exists(os.path.join(basedir,"resultado.zip")): os.remove(os.path.join(basedir,"resultado.zip"))
     return render_template('index.html')
 
 @app.route('/download')
@@ -62,13 +62,13 @@ def download_file():
 @app.route("/send", methods=['GET', 'POST'])
 def send():
     if request.method == "POST":
-        uploadedfiles = [f for f in listdir('uploads/') if isfile(join('uploads/', f))]
+        uploadedfiles = [f for f in listdir(os.path.join(basedir,'uploads/')) if isfile(join(os.path.join(basedir,'uploads/'), f))]
         if comprobar_envio(request.files["file"].filename,uploadedfiles):
             watermfile = request.files["file"]
-            watermfile.save(os.path.join("watermark/", watermfile.filename))
+            watermfile.save(os.path.join(os.path.join(basedir,"watermark/"), watermfile.filename))
             for file in uploadedfiles:
-                put_watermark('uploads/'+file,'output/watermark_' + file + '','watermark/'+watermfile.filename)
-            shutil.make_archive('resultado', 'zip', 'output/')
+                put_watermark(os.path.join(basedir,'uploads/'+file),os.path.join(basedir,'output/watermark_' + file),os.path.join(basedir,'watermark/'+watermfile.filename))
+            shutil.make_archive('resultado', 'zip', os.path.join(basedir,'output/'))
             return render_template('result.html')
     flash("Debe seleccionar una marca de agua y subir todos sus archivos en PDF")
     return redirect("/")
